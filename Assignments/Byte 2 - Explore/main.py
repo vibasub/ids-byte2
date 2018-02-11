07c07dc7262d688c9aaaa09ffcbe06bcfb32b098
Viba Subramaniam <viba.subramaniam@gmail.com>
2018-02-11 17:20:10 -0500

    First commit

--- Assignments/Byte 2 - Explore/main.py
+++ Assignments/Byte 2 - Explore/main.py
@@ -24,14 +24,15 @@ JINJA_ENVIRONMENT = jinja2.Environment(
     autoescape=True)
 
 # This API key is provided by google as described in the tutorial
-API_KEY = 'XXxxXxXXXXxxNXXxXXXxxxNNXXxxxxxxxXXXxXX'
+API_KEY = 'AIzaSyD7-s34IPH9FexKx24wkaZTGrYd-M1OFY4'
+
 
 # This uses discovery to create an object that can talk to the 
 # fusion tables API using the developer key
 service = build('fusiontables', 'v1', developerKey=API_KEY)
 
 # This is the table id for the fusion table
-TABLE_ID = 'NxxxNXxXxxNxXXXXNXxXXXxXxxxNxXxNxXxxXxxX'
+TABLE_ID = '1VlPiBCkYt_Vio-JT3UwM-U__APurJvPb6ZEJPg'
 
 # This is the default columns for the query
 query_cols = []
